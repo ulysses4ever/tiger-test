4e0749400312fb66c460c36a7ad734029194fc2d
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2019-10-04 18:30:02 -0400

    diffing complete

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,114 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns      #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Main where
 
 import Turtle hiding (textToLine)
 import Prelude hiding (FilePath)
+import Data.Char (isAlpha, isDigit)
 import Data.Either (fromRight)
 import Data.Maybe (fromJust)
 import Data.List (isPrefixOf)
 import qualified Control.Foldl as Fold
 import qualified Data.Text as TS
+import Data.String.Interpolate (i)
 
 main :: IO ()
-main = 
-  (sh $ do
-    prepareSubmissionForTest
+main = do
+  collectOutputs
+  print "DONE: recording output."
+  checkOutputs
+  print "DONE: testing."
 
-    -- Run a submission over what should lex and record results in "_out" subdir
-    test <- tigerFiles testsShouldWorkDir
-    _ <- recordOutput test
-    return ()) *> print "Done testing."
+{-
+--  Comparing results
+-}
+
+type LineNum = Int
+data AlignResult 
+    = Match
+    | NotMatch LineNum Text {- <- reference -} Text {- <- actual -}
+    | Truncated
+
+align :: Int -> [Text] -> [Text] -> AlignResult
+align _n [] _actual = Match
+align _n _refecernce [] = Truncated
+align n (r:rs) (a:as) =
+  case matchLines n r a of
+    Match -> align (n+1) rs as
+    nm@(NotMatch _ _ _) -> nm
+
+matchLines :: Int -> Text -> Text -> AlignResult
+matchLines n (firstToken -> r) (firstToken -> a)
+    | r == a    = Match
+    | otherwise = NotMatch n r a
+
+firstToken :: Text -> Text
+firstToken = TS.toUpper . TS.takeWhile isAlpha
+
+prepareTesting :: Shell ()
+prepareTesting = do
+    rm reportFilename
+    storeSubmId
+
+storeSubmId :: Shell ()
+storeSubmId = append reportFilename sudmId
+  where 
+    sudmId :: Shell Line
+    sudmId = textToLine
+           . TS.dropWhile isDigit
+           . TS.takeWhile (/= '/')
+           . TS.drop (TS.length (pathToText submDir) + 1)
+           . pathToText 
+           <$> pwd
+
+storeResult :: FilePath -> AlignResult -> Shell ()
+storeResult _ Match = return ()
+storeResult test Truncated = append reportFilename report 
+  where
+    report = return . stringToLine $
+                pathToString test ++ ": truncated"
+storeResult test (NotMatch line ref act) = append reportFilename report 
+  where
+    report = return . stringToLine $
+      [i|#{pathToString test}: line #{show line}: expected "#{ref}", actual "#{act}"|]
+
+compareWithReference :: Shell ()
+compareWithReference = do
+    outFile <- find (suffix ".out") outDir
+    actOut <- liftIO $ readTextFile outFile
+    refOut  <- liftIO $ readTextFile (referenceDir </> filename outFile)
+    storeResult (filename outFile) $ align 1 (TS.lines refOut) (TS.lines actOut)
+
+-- TODO: check no .err files in the should_work path
+checkNoDotErrFiles = return () -- undefined
+
+checkOutputs :: IO ()
+checkOutputs = sh $ do
+    enumerateSubmissions
+    prepareTesting
+    compareWithReference
+    checkNoDotErrFiles
+
+{-
+--  Recording outputs
+-}
 
 -- enumerating all submissions and for each one:
--- cd into it, cp the driver (run.sml) into it, create `_out` subdir
-prepareSubmissionForTest :: Shell ()
-prepareSubmissionForTest = do
+enumerateSubmissions :: Shell ()
+enumerateSubmissions = do
     aSubmDir <- ls submDir
     aSubm <- find (suffix "sources.cm") aSubmDir
     let implDir = directory aSubm
     cd implDir
+    liftIO . print $ "Processing " ++ (pathToString aSubmDir)
+
+-- cd into it, cp the driver (run.sml) into it, create `_out` subdir
+prepareCollectingOutput :: Shell ()
+prepareCollectingOutput = do
     cp runScript "./run.sml"
     mktree outDir
-    liftIO . print $ "Processing " ++ (pathToString aSubmDir)
 
 -- record output of a submission stored at FilePath, when run of the whole suite
 recordOutput :: FilePath -> Shell ()
@@ -40,8 +118,24 @@ recordOutput test = do
       procStrictWithErr "sml" ["run.sml",  pathToText test] (select [])
     let linesOut = select . map textToLine . dropWhileNotStart . TS.lines $ outRaw
         linesErr = select . map textToLine . TS.lines $ errRaw
-    output (outFileName ".out" test) linesOut
+    let outFile = (outFileName ".out" test)
+    touch outFile
+    output outFile linesOut
     when (not . TS.null $ errRaw) $ output (outFileName ".err" test) linesErr
+
+collectOutputs :: IO ()
+collectOutputs = sh $ do
+    enumerateSubmissions
+    prepareCollectingOutput
+
+    -- Run a submission over what should lex and record results in "_out" subdir
+    test <- tigerFiles testsShouldWorkDir
+    _ <- recordOutput test
+    return ()
+
+{-
+--  Helpers
+-}
 
 -- cuts head of stream of lines from stderr/stdout of a submission,
 -- while not see the start_lexer marker in the stdout (the second stream)
@@ -60,14 +154,21 @@ outFileName ext inp = outDir </> textToPath (inpFName `TS.append` ext)
 --       Hard-coded paths and constants
 -}
 
-baseDir :: String
-baseDir = "/home/ulysses/c/cs6410-compilers-TA"
-
 outDir :: IsString s => s
 outDir = "_out"
 
 phase :: IsString s => s
 phase = "lex"
+
+reportFilename :: IsString s => s
+reportFilename = "report.txt"
+
+baseDir :: String
+baseDir = "/home/ulysses/Documents/classes/cs6410-compilers-TA"
+
+referenceDir :: FilePath
+referenceDir = decodeString $
+  baseDir ++ "/submissions/a1/_MINE/_out"
 
 submDir :: FilePath
 submDir = decodeString $
@@ -103,14 +204,17 @@ textToLine = unsafeTextToLine
 stringToPath :: String -> FilePath
 stringToPath = decodeString
 
-pathToString :: FilePath -> String
-pathToString = encodeString
-
 textToPath :: Text -> FilePath
 textToPath = fromText
 
 pathToText :: FilePath -> Text
 pathToText = fromRight "ERRORING on FilePath->Text conv" . toText
+
+stringToText :: String -> Text
+stringToText = TS.pack
+
+pathToString :: FilePath -> String
+pathToString = encodeString
 
 {-
 newtype Comment = C [CommentContent]
